5031d8773e6a1e3d0cb6f925d9dad24c96bae8de
gorbachev_anton@geos.local <gorbachev_anton@smartworld.team>
2023-11-23 18:04:14 +0700

    feat: add userId, platform and authDate info to url

--- src/modules/game/game.ts
+++ src/modules/game/game.ts
@@ -2,15 +2,23 @@ import { Composer } from "grammy";
 import { COMMAND_PLAY_GAME } from "../../utils/commands";
 import { ContextWithI18n } from "../../types/context-with-i18n";
 import { config } from "../../config/config";
+import { URL } from "url";
 
 const gameModule = new Composer<ContextWithI18n>();
 
-gameModule.on("callback_query:game_short_name", (ctx) =>
-  ctx.answerCallbackQuery({
-    url: config.APP_GAME_CLIENT_URL,
-  })
-);
+gameModule.on("callback_query:game_short_name", (ctx) => {
+  const url = new URL(config.APP_GAME_CLIENT_URL);
+  url.searchParams.append("userId", ctx.from.id.toString());
+  url.searchParams.append("platform", "telegram");
+  url.searchParams.append("authDateTime", new Date().getTime().toString());
 
-gameModule.command(COMMAND_PLAY_GAME, (ctx) => ctx.replyWithGame(config.APP_CCG_GAME_NAME));
+  return ctx.answerCallbackQuery({
+    url: url.toString(),
+  });
+});
+
+gameModule.command(COMMAND_PLAY_GAME, (ctx) =>
+  ctx.replyWithGame(config.APP_CCG_GAME_NAME, {})
+);
 
 export default gameModule;
