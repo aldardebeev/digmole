05a67af4ad4b517c70e106775d919110ab022b32
Anton Gorbachev <gorbachev_anton@smartworld.team>
2023-11-30 14:40:00 +0700

    feat: add menu plugin for instructions

--- src/modules/instructions/instructions.ts
+++ src/modules/instructions/instructions.ts
@@ -1,31 +1,23 @@
 import { Composer, InlineKeyboard, Keyboard } from "grammy";
 import { INSTRUCTIONS_COMMAND } from "../../utils/commands";
 import { ContextWithI18n } from "../../types/context-with-i18n";
+import { Menu } from "@grammyjs/menu";
 
 const linkModule = new Composer<ContextWithI18n>();
 
 const connectMetamask = 'connect_metamask';
 const linkTelegramAccount = 'link_telegram_account';
 
+const menu = new Menu("instructions-menu")
+.text("Подключение MetaMask", (ctx) => ctx.reply("https://telegra.ph/Podklyuchenie-MetaMask-11-30")).row()
+.text("Привязка аккаунта к Telegram", (ctx) => ctx.reply("https://telegra.ph/Privyazka-akkaunta-k-Telegram-11-30"));
+
+linkModule.use(menu);
+
 linkModule.command(INSTRUCTIONS_COMMAND, async (ctx) => {
-    const keyboard = new InlineKeyboard()
-    keyboard.text('Подключение MetaMask', connectMetamask).row()
-    keyboard.text('Привязка аккаунта к Telegram', linkTelegramAccount)
-    await ctx.reply('Инструкции', {
-        reply_markup: keyboard,
+    await ctx.reply('Инструкции: ', {
+        reply_markup: menu,
     })
-})
-
-linkModule.callbackQuery(connectMetamask, async (ctx) => {
-    await ctx.answerCallbackQuery({
-        text: 'https://telegra.ph/Podklyuchenie-MetaMask-11-30',
-    });
-})
-
-linkModule.callbackQuery(linkTelegramAccount, async (ctx) => {
-    await ctx.answerCallbackQuery({
-        text: 'https://telegra.ph/Privyazka-akkaunta-k-Telegram-11-30',
-    });
 })
 
 export default linkModule;
