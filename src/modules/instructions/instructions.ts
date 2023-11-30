790e8b9ebd882387b46384ea1983084392f291b4
Anton Gorbachev <gorbachev_anton@smartworld.team>
2023-11-30 14:24:01 +0700

    fix: add return for answer

--- src/modules/instructions/instructions.ts
+++ src/modules/instructions/instructions.ts
@@ -8,17 +8,17 @@ linkModule.command(INSTRUCTIONS_COMMAND, async (ctx) => {
     const keyboard = new InlineKeyboard()
     keyboard.text('Подключение MetaMask', '/connect_metamask').row()
     keyboard.text('Привязка аккаунта к Telegram', '/link_telegram_account')
-    ctx.reply('Инструкции', {
+    return ctx.reply('Инструкции', {
         reply_markup: keyboard,
     })
 })
 
 linkModule.command('connect_metamask', async (ctx) => {
-    ctx.reply('https://telegra.ph/Podklyuchenie-MetaMask-11-30');
+    return ctx.reply('https://telegra.ph/Podklyuchenie-MetaMask-11-30');
 })
 
 linkModule.command('link_telegram_account', async (ctx) => {
-    ctx.reply('https://telegra.ph/Privyazka-akkaunta-k-Telegram-11-30');
+    return ctx.reply('https://telegra.ph/Privyazka-akkaunta-k-Telegram-11-30');
 })
 
 export default linkModule;
