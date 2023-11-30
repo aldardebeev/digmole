4ea310e5179007edb215f71084809bb16ad0ecfe
Anton Gorbachev <gorbachev_anton@smartworld.team>
2023-11-30 14:27:53 +0700

    fix: change reply on callbackQuery

--- src/modules/instructions/instructions.ts
+++ src/modules/instructions/instructions.ts
@@ -4,21 +4,24 @@ import { ContextWithI18n } from "../../types/context-with-i18n";
 
 const linkModule = new Composer<ContextWithI18n>();
 
+const connectMetamask = 'connect_metamask';
+const linkTelegramAccount = 'link_telegram_account';
+
 linkModule.command(INSTRUCTIONS_COMMAND, async (ctx) => {
     const keyboard = new InlineKeyboard()
-    keyboard.text('Подключение MetaMask', '/connect_metamask').row()
-    keyboard.text('Привязка аккаунта к Telegram', '/link_telegram_account')
-    return ctx.reply('Инструкции', {
+    keyboard.text('Подключение MetaMask', connectMetamask).row()
+    keyboard.text('Привязка аккаунта к Telegram', linkTelegramAccount)
+    await ctx.reply('Инструкции', {
         reply_markup: keyboard,
     })
 })
 
-linkModule.command('connect_metamask', async (ctx) => {
-    return ctx.reply('https://telegra.ph/Podklyuchenie-MetaMask-11-30');
+linkModule.callbackQuery(connectMetamask, async (ctx) => {
+    await ctx.answerCallbackQuery('https://telegra.ph/Podklyuchenie-MetaMask-11-30');
 })
 
-linkModule.command('link_telegram_account', async (ctx) => {
-    return ctx.reply('https://telegra.ph/Privyazka-akkaunta-k-Telegram-11-30');
+linkModule.callbackQuery(linkTelegramAccount, async (ctx) => {
+    await ctx.answerCallbackQuery('https://telegra.ph/Privyazka-akkaunta-k-Telegram-11-30');
 })
 
 export default linkModule;
